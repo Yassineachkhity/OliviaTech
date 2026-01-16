106bfe095479fad1d121d2d1b6d0fac01439a54a
0p3nEyE_X42 <125568814+Yassineachkhity@users.noreply.github.com>
2026-01-16 23:48:52 +0100

    Remove IncubatorSection from App component

--- oliviatech-landing/src/App.tsx
+++ oliviatech-landing/src/App.tsx
@@ -28,7 +28,6 @@ const App: React.FC = () => {
             <TeamSection />
             <CTASection />
           </main>
-          <IncubatorSection />
           <Footer />
         </div>
       </ThemeProvider>
